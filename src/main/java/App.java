1c14a23f7a41c8416999e690c77fe1e7212c9272
Kyle Tenzeldam and Ross Fletcher <ross@>
2018-01-11 08:29:45 -0800

    begin work on frontend / app.java

---
+++ src/main/java/App.java
@@ -0,0 +1,35 @@
+import java.lang.reflect.Array;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.HashMap;
+
+import models.Grocery;
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+import static spark.Spark.*;
+
+public class App {
+    public static void main(String[] args) {
+        staticFileLocation("/public");
+
+        get("/", (req, res) -> {
+            Map<String, Object> model = new HashMap<>();
+            ArrayList<Grocery> groceries = Grocery.getAll();
+            models.put("groceries", groceries);
+
+            return new ModelAndView(model, "index.hbs");
+        }, new HandlebarsTemplateEngine());
+
+        post("/post/new",  (request, response)  -> {
+            Map<String, Object> model = new HashMap<>();
+
+            String item = request.queryParams("item");
+            String quantity = request.queryParams("quantity");
+            String price = request.queryParams("price");
+
+            Grocery newGrocery = new Grocery(item, quantity, price);
+            return new ModelAndView(model, "success.hbs");
+        })
+    }
+}
