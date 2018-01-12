08b7e005f8f9514ef8bdb098cc3eb8bfd15a663d
Kyle Tenzeldam and Ross Fletcher <ross@>
2018-01-11 16:35:46 -0800

    add route for success hbs file

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -6,6 +6,8 @@ import java.util.HashMap;
 
 import models.Grocery;
 import spark.ModelAndView;
+import spark.Request;
+import spark.Response;
 import spark.template.handlebars.HandlebarsTemplateEngine;
 import static spark.Spark.*;
 
@@ -13,36 +15,37 @@ public class App {
     public static void main(String[] args) {
         staticFileLocation("/public");
 
-        get("/groceries/new", (req, res) -> {
-            Map<String, Object> model = new HashMap<>();
-            return new ModelAndView(model, "newpost-form.hbs");
-        }, new HandlebarsTemplateEngine());
-
-
-
         get("/", (req, res) -> {
             Map<String, Object> model = new HashMap<>();
             ArrayList<Grocery> groceries = Grocery.getAll();
             model.put("groceries", groceries);
-
             return new ModelAndView(model, "index.hbs");
         }, new HandlebarsTemplateEngine());
 
-        post("/groceries/new",  (req, res)  -> {
+        post("/groceries/new", (request, response) -> { //URL to make new post on POST route
             Map<String, Object> model = new HashMap<>();
 
-            String item = req.queryParams("item");
-            int quantity = Integer.parseInt(req.queryParams("quantity"));
-            Float price = Float.parseFloat(req.queryParams("price"));
-
-
-            Grocery newGrocery = new Grocery(item, quantity, price);
-            model.put("post", newGrocery);
-            model.put("item", newGrocery);
-            model.put("quantity", newGrocery);
-            model.put("price", newGrocery);
+            String item = request.queryParams("item");
+            Integer quantity = Integer.parseInt(request.queryParams("quantity"));
+            Float price = Float.parseFloat(request.queryParams("price"));
+            Grocery newGroceryItem = new Grocery(item, quantity, price);
+            model.put("GroceryItem", newGroceryItem);
             return new ModelAndView(model, "success.hbs");
         }, new HandlebarsTemplateEngine());
+
+
+
+//        get("/groceries/new", (req, res) -> {
+//            Map<String, Object> model = new HashMap<>();
+//            return new ModelAndView(model, "newpost-form.hbs");
+//        }, new HandlebarsTemplateEngine());
+
+
+
+
+
+
+
 
     }
 }
